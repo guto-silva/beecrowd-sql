0b20112d7ed9719a22a03a44c40009fbc154956d
Carlos Silva <carlosac_silva@yahoo.com.br>
2021-11-11 01:20:07 -0300

    Exercícios resolvidos 2607 e 2608.

---
+++ Bee2608.sql
@@ -0,0 +1,6 @@
+/*
+O setor financeiro da nossa empresa, está querendo saber os menores e maiores valores dos produtos, que vendemos.
+
+Para isso exiba somente o maior e o menor preço da tabela produtos.
+*/
+select max(price), min(price) from products;
